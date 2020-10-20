52713c83f2c3b3f24ddef14c1e2a832c301c1c55
Shreya414 <59734358+Shreya414@users.noreply.github.com>
2020-10-20 12:26:46 +0530

    Add files via upload

---
+++ Calc1.py
@@ -0,0 +1,15 @@
+
+def add():
+    print("Result1 is",__name__)
+
+def sub():
+    print("Result2 is")
+
+
+def main():
+    print("In Calc1 main")
+    add()
+    sub()
+
+if __name__ == "__main__":
+    main()
